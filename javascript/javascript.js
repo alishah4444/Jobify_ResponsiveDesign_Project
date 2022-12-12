f761d57b26f252d7d5044f92f44a866c5ee734b2
Alishah <alishahmaknojia.97@gmail.com>
2022-12-12 15:26:15 -0500

    push wrok done by me all

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -41,4 +41,19 @@ function enablehide() {
 
 const signUp = document.querySelector('.jobify_navbar-sign button');
 
-signUp.addEventListener('click', () => alert('Asfs'));
+signUp.addEventListener('click', () => window.location.href('../container/registration.html'));
+var coll = document.getElementsByClassName('collapsible');
+var i;
+
+for (i = 0; i < coll.length; i++) {
+	coll[i].addEventListener('click', function () {
+		this.classList.toggle('active');
+		var content = this.nextElementSibling;
+		if (content.style.display === 'block') {
+			content.style.display = 'none';
+		} else {
+			content.style.display = 'block';
+			document.querySelector('.blog1 p').scrollIntoView();
+		}
+	});
+}
